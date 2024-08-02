69be1c258cd912bd33be941db47523616cc2fcea
Paulo Junior <paulojpjunior@gmail.com>
2024-08-02 20:53:12 -0300

    food
    Primeiros passos do menu de navegação do Sidebar.

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,22 @@
-import { Container } from "./styles"
+import { Container } from './styles'
+
+import menuImg from '../../assets/menu.svg'
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
 
 export function Sidebar() {
-  return <Container>Sidebar</Container>
+  return (
+    <Container>
+      <button type='button'>
+        <img src={menuImg} alt='Abrir e fechar omenu' />
+      </button>
+      <nav>
+        <li>
+          <a href='#'>
+            <BurgerIcon />
+            <span>Hambúrguers</span>
+          </a>
+        </li>
+      </nav>
+    </Container>
+  )
 }
