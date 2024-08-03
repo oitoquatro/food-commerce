6e2cdaf2c139a0e299a93b248ba91e03d216e288
Paulo Junior <paulojpjunior@gmail.com>
2024-08-03 11:47:47 -0300

    food
    Aplicação dos demais icones do Sidebar.

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,11 @@
 import { Container } from './styles'
 
 import menuImg from '../../assets/menu.svg'
+
 import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaIcon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
 
 export function Sidebar() {
   return (
@@ -15,6 +19,24 @@ export function Sidebar() {
             <a href='#' className='active'>
               <BurgerIcon />
               <span>Hambúrguers</span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <PizzaIcon />
+              <span>Pizzas</span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <SodaIcon />
+              <span>Bebidas</span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <IceCreamIcon />
+              <span>Sobremesas</span>
             </a>
           </li>
         </ul>
