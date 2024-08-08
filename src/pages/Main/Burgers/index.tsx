ca8005ac3b4b8e6aeba2d92786a7b92607ac8e5a
Paulo Junior <paulojpjunior@gmail.com>
2024-08-08 08:35:31 -0300

    food
    Inicio da estratégia para o consumo de dados.
    Criação de um Array fixo.

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -2,6 +2,29 @@ import { Cabecalho } from '../../../components/Cabecalho'
 import { SnackTitle } from '../../../components/SnackTitle'
 
 export default function Burgers() {
+  const data = [
+    {
+      id: 1,
+      snack: 'burger',
+      name: 'Mega',
+      description:
+        'O artesanal tamanho família recheado com três carnes suculentas, queijo e bacon.',
+      price: 25.5,
+      image: 'https://i.imgur.com/upjIUnG.jpg',
+    },
+    {
+      id: 2,
+      snack: 'burger',
+      name: 'Extra bacon',
+      description:
+        'Criado para os amantes de bacon, possui em todas as suas camadas bacon bem assado e ainda queijo e carne.',
+      price: 23.5,
+      image: 'https://i.imgur.com/B4J04AJ.jpg',
+    },
+  ]
+
+  console.log('data', data)
+
   return (
     <>
       <Cabecalho title='Búrguers' />
