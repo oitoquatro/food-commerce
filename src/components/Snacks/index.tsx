6c774f7581072b115f1b7808ebfc24169bf58255
Paulo Junior <paulojpjunior@gmail.com>
2024-08-12 17:07:15 -0300

    food
    Criando um Loading básico para mostrar que a página está sendo carregada.

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -10,19 +10,23 @@ interface SnacksProps {
 export function Snacks({ snacks }: SnacksProps) {
   return (
     <Container>
-      {snacks.map((snack) => (
-        <div key={snack.id} className='snack'>
-          <h2>{snack.name}</h2>
-          <img src={snack.image} alt={snack.name} />
-          <p>{snack.description}</p>
-          <div>
-            <strong>{currencyFormat(snack.price)}</strong>
-            <button type='button'>
-              <FiPlus />
-            </button>
+      {!snacks.length ? (
+        <p style={{ color: '#fff' }}>Loading...</p>
+      ) : (
+        snacks.map((snack) => (
+          <div key={snack.id} className='snack'>
+            <h2>{snack.name}</h2>
+            <img src={snack.image} alt={snack.name} />
+            <p>{snack.description}</p>
+            <div>
+              <strong>{currencyFormat(snack.price)}</strong>
+              <button type='button'>
+                <FiPlus />
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </Container>
   )
 }
