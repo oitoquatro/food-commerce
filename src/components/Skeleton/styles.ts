b8e0638dd2d9df54c7f9fd3f4fcb5c35b88ff1bf
Paulo Junior <paulojpjunior@gmail.com>
2024-08-12 18:10:23 -0300

    food
    Finalizando o elemento de efeito de carregamento do Loading.

--- src/components/Skeleton/styles.ts
+++ src/components/Skeleton/styles.ts
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 import { SkeletonProps } from '.'
 
 const shimmerEffect = keyframes`
@@ -27,4 +27,38 @@ export const SkeletonElement = styled.div<SkeletonProps>`
   border-radius: 4px;
 
   animation: ${shimmerEffect} 1s linear infinite;
+
+  ${({ type }) =>
+    type === 'title' &&
+    css`
+      width: 75%;
+      height: 32px;
+      margin: 0.5rem 0;
+    `}
+
+  ${({ type }) =>
+    type === 'text' &&
+    css`
+      width: 100%;
+      height: 18px;
+      margin: 0.25rem 0;
+    `}
+
+    ${({ type }) =>
+    type === 'image' &&
+    css`
+      width: 200px;
+      height: 200px;
+      margin: 0.25rem 0;
+      border-radius: 50%;
+    `}
+
+    ${({ type }) =>
+    type === 'thumbnail' &&
+    css`
+      width: 100%;
+      height: 200px;
+      margin: 0%.5rem 0;
+      border-radius: 10px;
+    `}
 `
--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -2,6 +2,7 @@ import { FiPlus } from 'react-icons/fi'
 
 import { Container } from './Styles'
 import { currencyFormat } from '../../helpers/currencyFormats'
+import { SkeletonSnack } from './SkeletonSnack'
 
 interface SnacksProps {
   snacks: any[] //"any" para evitar o erro de id por hora
@@ -10,23 +11,21 @@ interface SnacksProps {
 export function Snacks({ snacks }: SnacksProps) {
   return (
     <Container>
-      {!snacks.length ? (
-        <p style={{ color: '#fff' }}>Loading...</p>
-      ) : (
-        snacks.map((snack) => (
-          <div key={snack.id} className='snack'>
-            <h2>{snack.name}</h2>
-            <img src={snack.image} alt={snack.name} />
-            <p>{snack.description}</p>
-            <div>
-              <strong>{currencyFormat(snack.price)}</strong>
-              <button type='button'>
-                <FiPlus />
-              </button>
+      {!snacks.length
+        ? [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
+        : snacks.map((snack) => (
+            <div key={snack.id} className='snack'>
+              <h2>{snack.name}</h2>
+              <img src={snack.image} alt={snack.name} />
+              <p>{snack.description}</p>
+              <div>
+                <strong>{currencyFormat(snack.price)}</strong>
+                <button type='button'>
+                  <FiPlus />
+                </button>
+              </div>
             </div>
-          </div>
-        ))
-      )}
+          ))}
     </Container>
   )
 }
