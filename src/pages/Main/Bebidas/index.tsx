9563bc9c25a5b529309d1e64bab8b18ce0809355
Paulo Junior <paulojpjunior@gmail.com>
2024-08-12 14:36:23 -0300

    food
    Aplicando a formatação e os itens em todas as pages.

--- src/pages/Main/Bebidas/index.tsx
+++ src/pages/Main/Bebidas/index.tsx
@@ -1,11 +1,43 @@
 import { Cabecalho } from '../../../components/Cabecalho'
+import { Snacks } from '../../../components/Snacks'
 import { SnackTitle } from '../../../components/SnackTitle'
 
 export default function Bebidas() {
+  const data = [
+    {
+      id: 1,
+      snack: 'drink',
+      name: 'Coca-Cola 2l',
+      description:
+        'A tradicional Coca-Cola que todo mundo adora.',
+      price: 12,
+      image: 'https://i.imgur.com/Lg3aKhf.jpg',
+    },
+    {
+      id: 2,
+      snack: 'drink',
+      name: 'Guaraná Antarctica',
+      description:
+        'O irresistível e saboroso Guaraná em sua versão de latinha.',
+      price: 6.5,
+      image: 'https://i.imgur.com/hOBrOIm.jpg',
+    },
+    {
+      id: 3,
+      snack: 'drink',
+      name: 'Suco de Abacaxí',
+      description:
+        'Suco natural de abacaxí com hortelã.',
+      price: 8,
+      image: 'https://i.imgur.com/VV9qTMh.jpg',
+    },
+  ]
+
   return (
     <>
       <Cabecalho title='Bebidas' />
       <SnackTitle>Bebidas</SnackTitle>
+      <Snacks snacks={data}></Snacks>
     </>
   )
 }
