c3d2c68a3be97b1f0048d9be33af4475ec4d9a16
Paulo Junior <paulojpjunior@gmail.com>
2024-08-20 18:36:14 -0300

    food
    Renderizando a quantidade de itens do carrinho ( qtd exata ).

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -12,27 +12,33 @@ interface SnacksProps {
 
 export function Snacks({ snacks }: SnacksProps) {
   //"useCart" está fazendo a chamada do "addSnackIntoCart"
-  const { addSnackIntoCart } = useCart()
+  const { cart, addSnackIntoCart } = useCart()
 
   return (
     <Container>
       {!snacks.length
         ? [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
-        : snacks.map((snack) => (
-            <div key={snack.id} className='snack'>
-              <span>1</span>
-              <h2>{snack.name}</h2>
-              <img src={snack.image} alt={snack.name} />
-              <p>{snack.description}</p>
-              <div>
-                <strong>{currencyFormat(snack.price)}</strong>
-                {/**"onClick" com a função de empilhar esperando add o snack */}
-                <button type='button' onClick={() => addSnackIntoCart(snack)}>
-                  <FiPlus />
-                </button>
+        : snacks.map((snack) => {
+            const snackExistent = cart.find(
+              (item) => item.snack === snack.snack && item.id === snack.id,
+            )
+            //pra executar qualquer código javascript dentro dessa função é preciso envolvelo com o return dentro da chaves.
+            return (
+              <div key={snack.id} className='snack'>
+                {snackExistent && <span>{snackExistent.quantity}</span>}
+                <h2>{snack.name}</h2>
+                <img src={snack.image} alt={snack.name} />
+                <p>{snack.description}</p>
+                <div>
+                  <strong>{currencyFormat(snack.price)}</strong>
+                  {/**"onClick" com a função de empilhar esperando add o snack */}
+                  <button type='button' onClick={() => addSnackIntoCart(snack)}>
+                    <FiPlus />
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            )
+          })}
     </Container>
   )
 }
