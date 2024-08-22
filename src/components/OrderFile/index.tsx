e289fd45cf7f78443e513dc93d05edefe346b273
Paulo Junior <paulojpjunior@gmail.com>
2024-08-22 08:40:07 -0300

    food
    Criação da logo de apresentação da quantidade de lanches da página de pedidos.

--- src/components/OrderFile/index.tsx
+++ src/components/OrderFile/index.tsx
@@ -1,13 +1,26 @@
 import { Link } from 'react-router-dom'
 import logoImg from '../../assets/logo.svg'
 import { Container } from './styles'
+import { useCart } from '../../hooks/useCart'
+import { ReactComponent as CartIcon } from '../../assets/shopping-cart.svg'
 
 export function OrderHeader() {
+  const { cart } = useCart()
+
   return (
     <Container>
       <Link to={'/'}>
         <img src={logoImg} alt='Food Commerce' />
       </Link>
+      <div>
+        <div>
+          <h3>Meus pedidos</h3>
+          <span>
+            <strong>{`${cart.length}`.padStart(2, '0')} lanche(s)</strong>
+          </span>
+        </div>
+        <CartIcon />
+      </div>
     </Container>
   )
 }
