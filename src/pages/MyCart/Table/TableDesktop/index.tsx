ba78f2963a4f7de573a24c7ba3027ada753da0a7
Paulo Junior <paulojpjunior@gmail.com>
2024-08-22 20:08:43 -0300

    food
    Construindo a tabela de produtos da pagina de pedidos.

---
+++ src/pages/MyCart/Table/TableDesktop/index.tsx
@@ -0,0 +1,41 @@
+import { currencyFormat } from '../../../../helpers/currencyFormats'
+import { useCart } from '../../../../hooks/useCart'
+import { Container } from './styles'
+
+export function TableDesktop() {
+  const { cart } = useCart()
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Lanche</th>
+            <th>Qtd</th>
+            <th>Subtotal</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((item) => (
+            <tr key={`${item.snack}-${item.id}`}>
+              <td>
+                <img src={item.image} alt={item.name} />
+              </td>
+              <td>
+                <h4>{item.name}</h4>
+                <span>{currencyFormat(item.price)}</span>
+              </td>
+              <td>{`${item.quantity}`.padStart(2, '0')}</td>
+              <td>
+                <h5>{currencyFormat(item.subtotal)}</h5>
+              </td>
+              <td>Deletar</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
+  )
+}
--- src/pages/MyCart/index.tsx
+++ src/pages/MyCart/index.tsx
@@ -1,12 +1,14 @@
 import { Cabecalho } from '../../components/Cabecalho'
 import { OrderHeader } from '../../components/OrderFile'
 import { Container } from './styles'
+import { Table } from './Table'
 
 export default function Main() {
   return (
     <Container>
       <Cabecalho title='carrinho' />
       <OrderHeader />
+      <Table />
     </Container>
   )
 }
