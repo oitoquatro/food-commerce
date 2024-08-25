b8b24a89d35bad9a5c5f19111d3aa0a49b480f92
Paulo Junior <paulojpjunior@gmail.com>
2024-08-25 18:41:25 -0300

    food
    Ajuste no Empty, comandos para quando não tiver itens no carrinho.

--- src/pages/MyCart/Table/index.tsx
+++ src/pages/MyCart/Table/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useCart } from '../../../hooks/useCart'
 import { TableDesktop } from './TableDesktop'
 import { TableMobile } from './TableMobile'
+import { EmptyCart } from '../../../components/EmptyCart'
 
 export function Table() {
   const [windowWidth, setWindowWidth] = useState(document.documentElement.clientWidth)
@@ -19,7 +20,7 @@ export function Table() {
     }
   }, [])
 
-  if (cart.length === 0) return <h1>Ops! Parece que você não tem pedidos, peça já!</h1>
+  if (cart.length === 0) return <EmptyCart title='Ops! Parece que você não tem pedidos, peça já!' />
 
   return windowWidth > 768 ? <TableDesktop /> : <TableMobile />
 }
