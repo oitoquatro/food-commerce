6a83d7a846799b8c533695c51203ea8986c48d8a
Paulo Junior <paulojpjunior@gmail.com>
2024-08-25 10:12:19 -0300

    food
    Ajuste do carrinho para Mobile.

--- src/pages/MyCart/Table/TableMobile/index.tsx
+++ src/pages/MyCart/Table/TableMobile/index.tsx
@@ -1,3 +1,45 @@
+import { currencyFormat } from '../../../../helpers/currencyFormats'
+import { useCart } from '../../../../hooks/useCart'
+import { Container } from './styles'
+import plusImg from '../../../../assets/circle-plus.svg'
+import minusImg from '../../../../assets/circle-minus.svg'
+import { FaTrashAlt } from 'react-icons/fa'
+import { ConfirmOrder } from '../../../../components/ConfirmOrder'
+
 export function TableMobile() {
-  return <h1>Mobile</h1>
+  const { cart, removeSnackFromCart, snackCartIncrement, snackCartDecrement } = useCart()
+
+  return (
+    <Container>
+      {cart.map((item) => (
+        <div key={`${item.snack}-${item.id}`} className='order-item'>
+          <div>
+            <img src={item.image} alt={item.name} />
+          </div>
+          <div>
+            <h4>{item.name}</h4>
+            <span>{currencyFormat(item.price)}</span>
+            <div>
+              <div>
+                <button type='button' onClick={() => snackCartDecrement(item)}>
+                  <img src={minusImg} alt='Remover quantidade' />
+                </button>
+                <span>{`${item.quantity}`.padStart(2, '0')}</span>
+                <button type='button' onClick={() => snackCartIncrement(item)}>
+                  <img src={plusImg} alt='Adicionar quantidade' />
+                </button>
+              </div>
+              <button type='button' onClick={() => removeSnackFromCart(item)}>
+                <FaTrashAlt />
+              </button>
+            </div>
+            <h5>
+              <span>Subtotal</span> {currencyFormat(item.subtotal)}
+            </h5>
+          </div>
+        </div>
+      ))}
+      <ConfirmOrder/>
+    </Container>
+  )
 }
